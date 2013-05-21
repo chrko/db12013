5e4dcb19160c3e7fef73a9ddfb59c20bf44c0db8
Christian Kohlstedde <christian@kohlsted.de>
2013-05-21 20:50:34 +0200

    only 1.7 missing

---
+++ sheet5/1.6.sql
@@ -0,0 +1,1 @@
+SELECT a1.name AS kuenstler, r1.name AS album, r1.year AS PubJahr FROM track AS t1, artist AS a1, artist_credit AS ac1, release AS r1 WHERE t1.name LIKE '%mal wieder richtig Sommer%' AND a1.id = ac1.artist AND ac1.tracklist = t1.tracklist AND ac1.pos = t1.pos AND t1.tracklist = r1.tracklist ORDER BY PubJahr DESC
