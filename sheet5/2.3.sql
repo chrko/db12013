33122c22937749ad69c72e3ea6a14d24fd680a62
Christian Kohlstedde <christian@kohlsted.de>
2013-05-23 09:58:37 +0200

    with having it's more beautiful

--- sheet5/2.3.sql
+++ sheet5/2.3.sql
@@ -1,1 +1,1 @@
-SELECT * FROM (SELECT country.name, COUNT(concert.*) as concert_count FROM country, concert WHERE country.id = concert.country GROUP BY country.name) AS concerts WHERE  concert_count >= 5000 ORDER BY name
+SELECT country.name, COUNT(concert.*) as concert_count FROM country, concert WHERE country.id = concert.country GROUP BY country.name HAVING COUNT(concert.*) >= 5000 ORDER BY country.name
