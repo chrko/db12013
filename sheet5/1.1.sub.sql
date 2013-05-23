44a54d098fc2dfc4086036a4574d730589be54c3
Christian Kohlstedde <christian@kohlsted.de>
2013-05-23 10:23:09 +0200

    1.1 divided into 2 files

--- sheet5/1.1.sub.sql
+++ sheet5/1.1.sub.sql
@@ -1,2 +1,1 @@
-SELECT con.name, con.eventdate FROM concert as con, country WHERE con.country = country.id AND country.name = 'Morocco' ORDER BY con.eventdate ASC
 SELECT con.name, con.eventdate FROM concert as con WHERE con.country = (SELECT country.id FROM country WHERE country.name = 'Morocco') ORDER BY con.eventdate ASC
