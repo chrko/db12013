c35d7a2d3288bcd140d55f8b35d686eb78af39b8
Christian Kohlstedde <christian@kohlsted.de>
2013-05-27 00:20:17 +0200

    fixed 1.7

--- sheet5/1.7.sql
+++ sheet5/1.7.sql
@@ -1,12 +1,23 @@
-SELECT DISTINCT a.name 
-FROM artist a, artist_relation ar
-WHERE a.id = ar.artist1
-    AND ar.type = 'member of band'
-    AND ar.artist0 IN (
-        SELECT DISTINCT ar0.artist0
+SELECT DISTINCT b.name
+FROM artist AS b,
+    artist_relation AS br1,
+    artist_relation AS br2, 
+    artist_relation AS br3,
+    (
+        SELECT DISTINCT ar0.artist0 AS artist0, ar0.artist1 AS artist1, ar1.artist1 AS artist2
         FROM artist_relation ar0, artist_relation ar1
         WHERE ar0.type = 'sibling'
             AND ar1.type = 'sibling'
             AND ar0.artist1 = ar1.artist0
             AND ar1.artist1 != ar0.artist0
-    )
+    ) AS s1
+WHERE br1.type = 'member of band'
+    AND br2.type = 'member of band'
+    AND br3.type = 'member of band'
+    AND br1.artist1 = br2.artist1
+    AND br2.artist1 = br3.artist1
+    AND br1.artist1 = b.id
+    AND br1.artist0 = s1.artist0
+    AND br2.artist0 = s1.artist1
+    AND br3.artist0 = s1.artist2
+ORDER BY b.name
