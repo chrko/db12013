03e79df0d1d3b85473ade4d816c6a7d2ac9df8f6
Christian Kohlstedde <christian@kohlsted.de>
2013-05-26 19:52:35 +0200

    einheitliches styling

--- sheet5/1.7.sql
+++ sheet5/1.7.sql
@@ -1,9 +1,12 @@
 SELECT DISTINCT a.name 
 FROM artist a, artist_relation ar
-WHERE a.id = ar.artist1 AND ar.type = 'member of band' AND ar.artist0 IN
-(
-	SELECT DISTINCT ar0.artist0
-	FROM artist_relation ar0, artist_relation ar1
-	WHERE ar0.type='sibling' AND ar1.type='sibling' AND ar0.artist1 = ar1.artist0 AND ar1.artist1 != ar0.artist0
-)
-;
+WHERE a.id = ar.artist1
+    AND ar.type = 'member of band'
+    AND ar.artist0 IN (
+        SELECT DISTINCT ar0.artist0
+        FROM artist_relation ar0, artist_relation ar1
+        WHERE ar0.type = 'sibling'
+            AND ar1.type = 'sibling'
+            AND ar0.artist1 = ar1.artist0
+            AND ar1.artist1 != ar0.artist0
+    )
