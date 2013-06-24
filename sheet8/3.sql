f07d0fe2ac66832119950e928ec66278f31c62e8
Christian Kohlstedde <christian@kohlsted.de>
2013-06-24 12:35:08 +0200

    improving the 3

--- sheet8/3.sql
+++ sheet8/3.sql
@@ -8,14 +8,18 @@ bm_agg(artists, artist1) AS (
     SELECT array_agg(bm.artist0), bm.artist1
     FROM bandmems AS bm
     GROUP BY bm.artist1
-    HAVING COUNT(bm.artist0) > 1)
-SELECT a1.name, a2.name
+    HAVING COUNT(bm.artist0) > 1),
+
+bnc(artists, oldid, newid) AS (
+SELECT bma1.artists, bma1.artist1, MIN(bma2.artist1)
 FROM bm_agg AS bma1,
-    bm_agg AS bma2,
-    artist AS a1,
-    artist AS a2
+    bm_agg AS bma2
 WHERE bma1.artists = bma2.artists
     AND bma1.artist1 < bma2.artist1
-    AND a1.id = bma1.artist1
-    AND a2.id = bma2.artist1
-ORDER BY bma1.artists, a1.name, a2.name
+GROUP BY bma1.artist1, bma1.artists)
+
+SELECT a1.name AS old, a2.name AS new
+FROM bnc, artist AS a1, artist AS a2
+WHERE a1.id = bnc.oldid
+AND a2.id = bnc.newid
+ORDER BY artists, oldid, newid
