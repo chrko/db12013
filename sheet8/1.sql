aece1dd8d4e1eb4991a55f19d70b007a0ca44dca
Christian Kohlstedde <christian@kohlsted.de>
2013-06-24 11:18:14 +0200

    sheet8 some sql

---
+++ sheet8/1.sql
@@ -0,0 +1,11 @@
+WITH concou(loc, count) AS
+    (SELECT "loc", count(*) AS "count"
+    FROM "concert"
+    WHERE loc IS NOT NULL
+    GROUP BY "loc"),
+    mostcons(loc, eventdate) AS
+    (SELECT concou.loc, eventdate
+    FROM concou, concert
+    WHERE count >= ALL (SELECT count FROM concou))
+SELECT * FROM mostcons
+WHERE eventdate <= ALL (SELECT eventdate FROM mostcons)
---
+++ sheet8/2.sql
@@ -0,0 +1,1 @@
+SELECT c1.loc, c1.city, COUNT(c1.*) AS count FROM concert AS c1, country WHERE c1.country = country.id AND country.name = 'United States' AND c1.loc IS NOT NULL GROUP BY c1.loc, c1.city ORDER BY count desc LIMIT 10
