423471c4820888ef4b88a8f476cec22eb4938e44
Christian Kohlstedde <christian@kohlsted.de>
2013-06-24 11:55:05 +0200

    styling the 2

--- sheet8/2.sql
+++ sheet8/2.sql
@@ -1,1 +1,8 @@
-SELECT c1.loc, c1.city, COUNT(c1.*) AS count FROM concert AS c1, country WHERE c1.country = country.id AND country.name = 'United States' AND c1.loc IS NOT NULL GROUP BY c1.loc, c1.city ORDER BY count desc LIMIT 10
+SELECT c1.loc, c1.city, COUNT(c1.*) AS count
+FROM concert AS c1, country
+WHERE c1.country = country.id
+    AND country.name = 'United States'
+    AND c1.loc IS NOT NULL
+GROUP BY c1.loc, c1.city
+ORDER BY count DESC
+LIMIT 10
