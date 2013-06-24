38a6b5893a5ff7c4b7e300a00a29c4cbbd9309d7
Christian Kohlstedde <christian@kohlsted.de>
2013-06-24 11:51:57 +0200

    styling 3 and result

--- sheet8/3.sql
+++ sheet8/3.sql
@@ -1,3 +1,21 @@
-WITH bandmems(artist0, artist1) AS (SELECT ar1.artist0, ar1.artist1 FROM artist_relation AS ar1 WHERE ar1.type = 'member of band' ORDER BY ar1.artist1, ar1.artist0),
-bm_agg(artists, artist1, count) AS (SELECT array_agg(bm.artist0), bm.artist1, COUNT(bm.artist0) FROM bandmems AS bm GROUP BY bm.artist1 HAVING COUNT(bm.artist0) > 1)
-SELECT bma1.artists, a1.name, a2.name FROM bm_agg AS bma1, bm_agg AS bma2, artist AS a1, artist AS a2 WHERE bma1.artists = bma2.artists AND bma1.artist1 < bma2.artist1 AND a1.id = bma1.artist1 AND a2.id = bma2.artist1 ORDER BY bma1.artists, a1.name, a2.name
+WITH bandmems(artist0, artist1) AS (
+    SELECT ar1.artist0, ar1.artist1
+    FROM artist_relation AS ar1
+    WHERE ar1.type = 'member of band'
+    ORDER BY ar1.artist1, ar1.artist0),
+
+bm_agg(artists, artist1) AS (
+    SELECT array_agg(bm.artist0), bm.artist1
+    FROM bandmems AS bm
+    GROUP BY bm.artist1
+    HAVING COUNT(bm.artist0) > 1)
+SELECT a1.name, a2.name
+FROM bm_agg AS bma1,
+    bm_agg AS bma2,
+    artist AS a1,
+    artist AS a2
+WHERE bma1.artists = bma2.artists
+    AND bma1.artist1 < bma2.artist1
+    AND a1.id = bma1.artist1
+    AND a2.id = bma2.artist1
+ORDER BY bma1.artists, a1.name, a2.name
